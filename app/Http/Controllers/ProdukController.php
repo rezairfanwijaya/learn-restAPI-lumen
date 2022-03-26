d62bd0f142a826aacff0fccc4336ea8571a0c46a
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-03-27 01:44:57 +0700

    filtering data

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -6,13 +6,22 @@ use App\Produk;
 use Illuminate\Http\Request;
 
 
+
+
 class ProdukController extends Controller
 {
     public function create (Request $request)
     {
-        // ambil data dari form
-        $data = $request->all();
-        $produk = Produk::create($data);
+        // ambil data dari form dan lakukan filtering
+        $this->validate($request, [
+            'nama' => 'required|string',
+            'harga' => 'required|integer',
+            'warna' => 'required|string',
+            'kondisi' => 'required|in:Baru,Lama',
+            'deskripsi' => 'string|nullable',
+        ]);
+        
+        $produk = Produk::create($request->all());
 
         // kita kasih return berupa json dari data tadi
         return response()->json($produk);
